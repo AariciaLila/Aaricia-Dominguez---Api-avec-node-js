64bba35feb4b8a54e8dbad1ab5ae76618e05e7ec
AariciaLila <82643809+AariciaLila@users.noreply.github.com>
2022-07-22 15:34:23 +0200

    Création du serveur Express - Projet Final
    Dans cette première partie du programme, on a :
    
    - configurer notre environnement de développement avec toutes les dépendances requises pour démarrer
    - démarrer notre serveur Node
    - gérer notre application Express
    - créer deux routes
    - implémenter le CORS pour s'assurer que le serveur pouvait effectuer des appels vers l'application en toute sécurité

---
+++ projet-api/models/user.js
@@ -0,0 +1,35 @@
+/**
+ * auth.js
+ * 
+ * Schéma de données des utilisateurs
+ */
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  status: {
+    type: String,
+    default: 'I am new!'
+  },
+  posts: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'Post'
+    }
+  ]
+});
+
+module.exports = mongoose.model('User', userSchema);
